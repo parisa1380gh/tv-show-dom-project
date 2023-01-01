065e8dcbc5cc79a02ffea44d8421e3493592889e
parisa1380gh <101459274+parisa1380gh@users.noreply.github.com>
2023-01-01 13:29:24 -0800

    p3

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 let da;
-const getShow = async() => {
+const getdata = async() => {
     let url = ("https://api.tvmaze.com/shows/82/episodes");
     const response = await axios.get(url);
     da = await response.data.map((x) => x);
     return response.data;
 };
 
-getShow()
+getdata()
     .then((data) => {
-        optionElement(data);
-        showsCard(data);
+        optionss(data);
+        cards(data);
     })
     .catch((err) => {
         console.log("rejected", err);
@@ -26,7 +26,7 @@ const container = document.querySelector(".container");
 
 
 
-const optionElement = (episodesList) => {
+const optionss = (episodesList) => {
     const option = document.createElement("option");
     option.append(`All Episods`);
     select.append(option);
@@ -38,13 +38,13 @@ const optionElement = (episodesList) => {
     });
 };
 
-const showsCard = (data) => {
+const cards = (data) => {
     data.forEach((episod) => {
-        createCard(episod);
+        card(episod);
     });
 };
 
-const createCard = (episod) => {
+const card = (episod) => {
     const div = document.createElement("div");
     const h3 = document.createElement("h3");
     const span = document.createElement("span");
@@ -72,11 +72,11 @@ const createCard = (episod) => {
 select.addEventListener("change", function(e) {
     if (e.target.value === "All Episods") {
         container.innerHTML = "";
-        showsCard(da);
+        cards(da);
     } else {
         container.innerHTML = "";
 
-        createCard(da[e.target.selectedIndex - 1]);
+        card(da[e.target.selectedIndex - 1]);
     }
 });
 
@@ -85,7 +85,7 @@ serach.addEventListener("input", async(e) => {
     container.innerHTML = "";
     da.forEach((ele) => {
         if (ele.name.toLowerCase().includes(e.target.value.toLowerCase())) {
-            createCard(ele);
+            card(ele);
         }
     });
 });
